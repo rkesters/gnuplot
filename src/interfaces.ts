82748191c40d6835c2e4eb8e9eb4bcfeff14fb71
Robert Kesterson <rkesters@gmail.com>
2020-11-25 03:44:21 -0500

    Updated to use Maps

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,46 +1,53 @@
 import { ExecException, ChildProcess } from "child_process";
 
 export interface SetUpOptions {
-    format?: any;
-    font?: any;
-    fontSize?: any;
-    width?: any;
-    height?: any;
-    locale?: any;
-    xRange?: any;
-    yRange?: any;
-    margin?: any;
-    time?: "hours" | "Hours" | "Days" | "days" | string;
-    title?: any;
-    titleSize?: any;
-    logscale?: any;
-    xlabel?: any;
-    ylabel?: any;
-    decimalsign?: any;
-    xRotate?: any;
-    yFormat?: any;
-    nokey?: any;
+  format?: any;
+  font?: any;
+  fontSize?: any;
+  width?: any;
+  height?: any;
+  locale?: any;
+  xRange?: any;
+  yRange?: any;
+  margin?: any;
+  time?: "hours" | "Hours" | "Days" | "days" | string;
+  title?: any;
+  titleSize?: any;
+  logscale?: any;
+  xlabel?: any;
+  ylabel?: any;
+  decimalsign?: any;
+  xRotate?: any;
+  yFormat?: any;
+  nokey?: any;
 }
 
-export type FinishFunc = (error: ExecException | null, stdout: string | Buffer, stderr: string | Buffer) => void;
+export type FinishFunc = (
+  error: ExecException | null,
+  stdout: string | Buffer,
+  stderr: string | Buffer
+) => void;
+
+export type DataType = Map<string | number, number>;
+export type SeriesDataType = { [name: string]: DataType };
+export type DataTypeInput =
+  | number[]
+  | { [name: string]: DataType | number[] }
+  | DataType;
 export interface PlotOptions extends SetUpOptions {
-    data?: any;
-    filename: string;
-    style?: string;
-    moving_avg?: number;
-    moving_max?: number;
-    exec?: any;
-    hideSeriesTitle?: boolean;
-    finish?: FinishFunc;
-    args?: string[]; // commands
-}
-
-export interface AsyncPlotOptions extends SetUpOptions {
-    data?: any;
-    filename: string;
-    style?: string;
-    moving_avg?: number;
-    moving_max?: number;
-    exec?: any;
-    hideSeriesTitle?: boolean;
+  data?: DataTypeInput;
+  filename: string;
+  style?:
+    | "lines"
+    | "impulses"
+    | "points"
+    | "linespoints"
+    | "candlesticks"
+    | string;
+  moving_avg?: number;
+  moving_max?: number;
+  exec?: any;
+  hideSeriesTitle?: boolean;
+  finish?: FinishFunc;
+  args?: string[]; // commands
 }
